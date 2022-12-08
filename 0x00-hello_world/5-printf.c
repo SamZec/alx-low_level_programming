26e6433ed07a1354d19e1ec28fb56d8feddc22a4
SamZec <samzec4@gmail.com>
2022-12-08 21:42:10 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char grammer[] =  "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", grammer);
+
+	return (0);
+}
