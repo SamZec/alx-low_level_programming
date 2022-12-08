69b4561e3f2b3e6c37cefe5464ca099ce9c1cc10
SamZec <samzec4@gmail.com>
2022-12-08 21:24:44 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ */
+
+int main(void)
+{
+	/*initailize puzzel to hold string*/
+	char puzzel[] = "\"Programming is like building a multilingual puzzle";
+
+	/*call puzzel with puts to print the string*/
+	puts(puzzel);
+
+	/*return 0 if success*/
+	return (0);
+}
