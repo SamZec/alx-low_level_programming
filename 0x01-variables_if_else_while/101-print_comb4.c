cebaf4e5323aabc32eed389db7ed41ff94d544a3
SamZec <samzec4@gmail.com>
2022-12-10 12:04:59 +0000

    11. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 48, j = 48, h = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			h = 48;
+			while (h < 58)
+			{
+				if ((i != j && i != h && j != h ) && (i < j && j < h))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(h);
+					if (j == 56 && i == 55 && h == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				h++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
