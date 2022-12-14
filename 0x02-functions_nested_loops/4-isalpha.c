a5b314bc6435a2cedae2492111f6290af339caed
SamZec <samzec4@gmail.com>
2022-12-14 02:31:30 +0000

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _isalpha - checks for a letter
+ * _isalpha - checks for a letter
  * @c: takes character to check
  *
  * Return: 1 if c is lower or upper character
