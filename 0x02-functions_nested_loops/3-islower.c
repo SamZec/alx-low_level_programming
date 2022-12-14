633e945f2151ea0adeecbe0de8d4c7e3cb53a639
SamZec <samzec4@gmail.com>
2022-12-14 01:39:47 +0000

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _islower - checks for lowercase character
+ *
+ * Return: 1 (success) 0 (failure)
+ */
+int _islower(int c)
+{
+	if ('a' <= c <= 'z')
+		return (1);
+	else
+		return (0);
+}
