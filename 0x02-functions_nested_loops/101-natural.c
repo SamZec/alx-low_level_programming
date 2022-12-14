16aace99712305c663757a1ede843bee34f7d458
SamZec <samzec4@gmail.com>
2022-12-14 21:36:51 +0000

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints multiples of 3 and 5
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	_putchar(sum);
+	_putchar('\n');
+	return (0);
+}
