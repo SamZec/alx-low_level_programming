0df7c045e422bdeaac9c6d93b21a072151abe8fd
SamZec <samzec4@gmail.com>
2022-12-19 23:26:57 +0000

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,11 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	for (j = 0; j <= i; j += 2)
+		_putchar(str[j]);
 	_putchar('\n');
 }
