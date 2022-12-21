5df00985e488fcd4d42b1854e605bece34590b67
SamZec <samzec4@gmail.com>
2022-12-21 18:08:52 +0000

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 (success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int rt;
+
+	rt = strcmp(s1, s2);
+	return (rt);
+}
