9c160bba04f7e5c757bbf67062b89b0c94de3afe
SamZec <samzec4@gmail.com>
2023-01-09 13:34:17 +0000

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - prints the number of arguments passed into it.
+ * main - prints the product of arguments passed into it.
  * @argc: arguments count
  * @argv: arguments array
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (success) otherwise 1
  */
 int main(int argc, char **argv)
 {
 	int mul;
 
-	if (argc <= 1)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
