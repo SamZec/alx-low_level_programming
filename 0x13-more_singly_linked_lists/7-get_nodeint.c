5d50a13744ba2e01c6bf93cc7e70eb8736c4c2ee
SamZec <samzec4@gmail.com>
2023-01-31 00:26:19 +0000

    7. Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
-	if (head == NULL)
-		return (NULL);
-
 	while (count < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		count++;
 	}
