7b8bf695225f2fb1a78e7c07f96367d6b45f4d2f
SamZec <samzec4@gmail.com>
2023-02-06 21:56:00 +0000

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len, w;
+	int fd, len, wr;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,8 +19,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (len = 0; text_content[len] != '\0'; )
 			len++;
-		w = write(fd, text_content, len);
-		if (w == -1)
+		wr = write(fd, text_content, len);
+		if (wr == -1)
 			return (-1);
 	}
 	close(fd);
