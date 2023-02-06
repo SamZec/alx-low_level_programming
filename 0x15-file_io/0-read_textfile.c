2126eca870f5770b84bc6fcd6c0d0aa103b48872
SamZec <samzec4@gmail.com>
2023-02-06 21:24:48 +0000

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,28 +12,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t op, rd, wr;
-	char *buff;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
-		return (0);
-
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-		return (0);
-
-	op = open(filename, O_RDONLY);
-	rd = read(o, buff, letters);
-	wr = write(STDOUT_FILENO, buff, r);
-
-	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
-		free(buff);
 		return (0);
 	}
 
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
 	free(buff);
-	close(op);
-
-	return (wr);
+	return (wd);
 }
