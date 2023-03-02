807fe80a25b610ec97fd5bf2b13dcbfbc234cf39
SamZec <samzec4@gmail.com>
2023-03-02 00:29:42 +0000

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,41 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - print first 98 Fibonacci numbers
+ * main - first 50 fibonatchi.
  *
- * Return: Always 0 (success)
+ * Return: void.
  */
 int main(void)
 {
-	unsigned long int j, k, add;
-	int i;
+	unsigned long int i, i0, i1, j, j0, j1, e, e0, e1, y, k;
 
-	j = add = 0;
-	k = 1;
+	i = 1;
+	j = 2;
+	e = i + j;
 
-	for (i = 0; i <= 97; i++)
+	printf("%lu, ", i);
+	printf("%lu, ", j);
+	for (y = 3; y < 89; y++)
 	{
-		add = j + k;
-		printf("%lu", add);
-		j = k;
-		k = add;
-		if (i == 97)
-			break;
-		printf(", ");
+		printf("%lu, ", e);
+		i = j;
+		j = e;
+		e = i + j;
 	}
-	printf("\n");
+	j0 = j / 1000000000;
+	j1 = j % 1000000000;
+	e0 = e / 1000000000;
+	e1 = e % 1000000000;
+	for (k = 89; k < 98; k++)
+	{
+		printf("%lu%lu, ", e0, e1);
+		i0 = j0;
+		i1 = j1;
+		j0 = e0;
+		j1 = e1;
+		e0 = i0 + j0 + ((i1 + j1) / 1000000000);
+		e1 = (i1 + j1) % 1000000000;
+	}
+printf("%lu%lu\n", e0, e1);
 	return (0);
 }
