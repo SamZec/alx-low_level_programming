8594c30b3f041714a0e9293961dfc4640b808363
SamZec <samzec4@gmail.com>
2023-03-16 15:58:03 +0000

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,62 +1,54 @@
 #include "lists.h"
 
-unsigned int len_node(dlistint_t **node);
-
 /**
- * insert_dnodeint_at_index - insert element at given index
- * @h:node
- * @idx:index
- * @n:element value
- * Return: NULL or New element
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c = 0;
-	dlistint_t *start, *new;
-	unsigned int len = len_node(h);
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
+	new = NULL;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-	start = *h;
-	while (start)
+		new = add_dnodeint(h, n);
+	else
 	{
-		if (c == idx - 1)
-			break;
-		else if (c < idx - 1 && start == NULL)
-		return (NULL);
-		start = start->next;
-		c++;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-			return (NULL);
-	if (len == idx)
-		return (add_dnodeint_end(h, n));
-	else if (len < idx)
-		return (NULL);
-	start->next->prev = new;
-	new->next = start->next;
-	start->next = new;
-	new->prev = start;
-	new->n = n;
-	return (new);
-}
-/**
- * len_node - list len
- *
- * @node:list
- * Return:unsigned int
- */
-unsigned int len_node(dlistint_t **node)
-{
-	unsigned int len = 0;
-	dlistint_t *start;
 
-	start = *node;
-	while (start != NULL)
-	{
-		len += 1;
-		start = start->next;
-	}
-	return (len);
+	return (new);
 }
