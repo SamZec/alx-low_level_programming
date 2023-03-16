2a6ed58dcd86262b52ddfc39aa185fa17d217c20
SamZec <samzec4@gmail.com>
2023-03-16 15:55:07 +0000

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,62 @@
 #include "lists.h"
 
+unsigned int len_node(dlistint_t **node);
+
 /**
- * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *                            list at a given position.
- * @h: A pointer to the head of the dlistint_t list.
- * @idx: The position to insert the new node.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ * insert_dnodeint_at_index - insert element at given index
+ * @h:node
+ * @idx:index
+ * @n:element value
+ * Return: NULL or New element
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h, *new;
+	unsigned int c = 0;
+	dlistint_t *start, *new;
+	unsigned int len = len_node(h);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
+	start = *h;
+	while (start)
 	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-	}
-
-	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+		if (c == idx - 1)
+			break;
+		else if (c < idx - 1 && start == NULL)
 		return (NULL);
-
+		start = start->next;
+		c++;
+	}
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+	if (len == idx)
+		return (add_dnodeint_end(h, n));
+	else if (len < idx)
+		return (NULL);
+	start->next->prev = new;
+	new->next = start->next;
+	start->next = new;
+	new->prev = start;
 	new->n = n;
-	new->prev = tmp;
-	new->next = tmp->next;
-	tmp->next->prev = new;
-	tmp->next = new;
-
 	return (new);
+}
+/**
+ * len_node - list len
+ *
+ * @node:list
+ * Return:unsigned int
+ */
+unsigned int len_node(dlistint_t **node)
+{
+	unsigned int len = 0;
+	dlistint_t *start;
+
+	start = *node;
+	while (start != NULL)
+	{
+		len += 1;
+		start = start->next;
+	}
+	return (len);
 }
