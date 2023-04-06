5e07b7eaa835e06641d94a3151b6234e425c9cae
SamZec <samzec4@gmail.com>
2023-04-06 23:28:08 +0000

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,15 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_table_t *h_table;
+	hash_node_t **_array;
 
 	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = malloc(sizeof(hash_node_t) * h_table->size);
+	_array = malloc(sizeof(hash_node_t *) * h_table->size);
 	if (h_table == NULL)
 		return (NULL);
 	for (i = 0; i < h_table->size; i++)
-		h_table->array[i] = NULL;
+		_array[i] = NULL;
+	h_table->array = _array;
 	return (h_table);
 }
