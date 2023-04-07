c70dd791cdd8741265586371b542d16eba01c150
SamZec <samzec4@gmail.com>
2023-04-07 13:44:06 +0000

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,23 +2,19 @@
 #include <string.h>
 
 /**
- * hash_table_set - a function that adds an element to the hash table.
- * @ht: hash table
+ * hashes - insert node into a hash table at index
+ *
+ * @head: pointer to node at index
  * @key: is the key
  * @value: value associated with key
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: pointer to node at index
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *hashes(hash_node_t **h_node, const char *key, const char *value)
 {
 	hash_node_t *n_hash_node;
-	unsigned long int index;
 
-	if (key == NULL || value == NULL || ht->size == 0)
-		return (0);
-
-	index = key_index((unsigned char *)key, ht->size);
-	n_hash_node = ht->array[index];
+	n_hash_node = *h_node;
 
 	while (n_hash_node != NULL)
 	{
@@ -26,17 +22,47 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(n_hash_node->value);
 			n_hash_node->value = strdup(value);
-			return (1);
+			return (*h_node);
 		}
 		n_hash_node = n_hash_node->next;
 	}
+
 	n_hash_node = malloc(sizeof(hash_node_t));
+
 	if (n_hash_node == NULL)
+		return (NULL);
+
+	n_hash_node->key = strdup(key);
+	n_hash_node->value = strdup(value);
+	n_hash_node->next = *h_node;
+	*h_node = n_hash_node;
+
+	return (*h_node);
+}
+
+/**
+ * hash_table_set - a function that adds an element to the hash table.
+ *
+ * @ht: is the hash table
+ * @key: is the key
+ * @value: value associated with key
+ *
+ * Return: 1 if successes, 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+
+	if (ht == NULL)
 		return (0);
-	strcpy(n_hash_node->key, key);
-	strcpy(n_hash_node->value, value);
-	n_hash_node->next = ht->array[index];
-	ht->array[index] = n_hash_node->next;
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (hashes(&(ht->array[index]), key, value) == NULL)
+		return (0);
 
 	return (1);
 }
